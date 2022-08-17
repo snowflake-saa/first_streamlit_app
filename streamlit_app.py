f519d113aa1633aa1cd8c676af03593821bf0009
Saurabh <111366813+snowflake-saa@users.noreply.github.com>
2022-08-17 21:48:42 +0530

    Update streamlit_app.py
    changes to show data in table form

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
+# take the json and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
+#output it to screen as a table
+streamlit.dataframe(fruityvice_normalized)
