4cda2779d4522fb05a30555e6ddcf795a774858a
Saurabh <111366813+snowflake-saa@users.noreply.github.com>
2022-08-22 12:17:50 +0530

    Update streamlit_app.py
    Insert data into snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,6 @@ if streamlit.button('Get Fruit Load list'):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
-streamlit.stop()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#streamlit.stop()
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 
